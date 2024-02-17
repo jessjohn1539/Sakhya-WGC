b8615d2fae26a61bb9bd2a37855f94f7a793c9cf
Jess John <jessjohn1539@gmail.com>
2024-02-17 14:04:21 +0530

    Added trustees images

---
+++ src/assets/images/Trustees/index.ts
@@ -0,0 +1,21 @@
+import President from "./president.png";
+import VicePresident from "./vice_president.png";
+import Treasurer from "./treasurer.png";
+import CoTrustee1 from "./co-trustee-1.png";
+import CoTrustee2 from "./co-trustee-2.png";
+import CoTrustee3 from "./co-trustee-3.png";
+import CoTrustee4 from "./co-trustee-4.png";
+import CoTrustee5 from "./co-trustee-5.png";
+import CoTrustee6 from "./co-trustee-6.png";
+
+export {
+  President,
+  VicePresident,
+  Treasurer,
+  CoTrustee1,
+  CoTrustee2,
+  CoTrustee3,
+  CoTrustee4,
+  CoTrustee5,
+  CoTrustee6,
+};
