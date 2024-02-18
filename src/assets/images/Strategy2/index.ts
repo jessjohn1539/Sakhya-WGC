38607ec993dcddf1099ba48e0508d2a404de45ac
Jess John <jessjohn1539@gmail.com>
2024-02-18 19:57:16 +0530

    Added images for Strategy2 page

---
+++ src/assets/images/Strategy2/index.ts
@@ -0,0 +1,21 @@
+import Community1 from './Community/Community-1.jpg'
+import Community2 from './Community/Community-2.jpg'
+import Community3 from './Community/Community-3.jpg'
+import TOT1 from './TOT/tot-1.jpg'
+import Exposure1 from './Exposure/Exposure-1.jpg'
+import Exposure2 from './Exposure/Exposure-2.jpg'
+import Students1 from './Students/Session-students-1.jpg'
+import Workshop1 from './Workshop/Workshop-1.jpg'
+import HarassmentSession1 from './HarassmentSession/Harassment-1.jpg'
+
+export {
+  Community1,
+  Community2,
+  Community3,
+  TOT1,
+  Exposure1,
+  Exposure2,
+  Students1,
+  Workshop1,
+  HarassmentSession1,
+}
