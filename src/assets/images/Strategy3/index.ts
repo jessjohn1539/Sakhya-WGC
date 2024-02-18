74e79d8e5f56ed5dcf5a5ae863791fd97d090a51
Jess John <jessjohn1539@gmail.com>
2024-02-19 00:25:14 +0530

    Added new Strategy3 campaign and street play images

---
+++ src/assets/images/Strategy3/index.ts
@@ -0,0 +1,13 @@
+import Campaign1 from './Campaign/campaign1.png'
+import Campaign2 from './Campaign/campaign2.png'
+import Campaign3 from './Campaign/campaign3.png'
+import StreetPlay1 from './StreetPlay/sp1.png'
+import StreetPlay2 from './StreetPlay/sp2.png'
+
+export {
+    Campaign1,
+    Campaign2,
+    Campaign3,
+    StreetPlay1,
+    StreetPlay2
+}
