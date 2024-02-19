ccf2d3d97b243f3140538e5860e9dc3c4b7653e9
Jess John <jessjohn1539@gmail.com>
2024-02-19 15:58:05 +0530

    Added new images for Strategy4

---
+++ src/assets/images/Strategy4/index.ts
@@ -0,0 +1,11 @@
+import Police1 from './Police/police1.png'
+import Police2 from './Police/police2.png'
+import Police3 from './Police/police3.png'
+import CHARI1 from './CHARI/CHARI1.png'
+
+export {
+    Police1,
+    Police2,
+    Police3,
+    CHARI1
+}
