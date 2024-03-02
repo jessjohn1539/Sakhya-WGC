3233e9e4cff7d7bf6527ad76e51ca8f0b2e6012b
Jess John <jessjohn1539@gmail.com>
2024-03-02 14:55:20 +0530

    Added new contributors and social icons

---
+++ src/assets/icons/socials/index.ts
@@ -0,0 +1,7 @@
+import facebook from './facebook.svg';
+import github from './github.svg';
+import instagram from './instagram.svg';
+import linkedin from './linkedin.svg';
+import globe from './globe.svg';
+
+export default { facebook, github, instagram, linkedin, globe };
